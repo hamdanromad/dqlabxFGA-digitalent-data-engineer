6f2c5b37e0db7f6012589c76ceb53b229eb71c0e
Hamdan Romadhona <104075365+hamdanromad@users.noreply.github.com>
2022-07-01 20:18:58 +0700

    Update data-manipulation-with-pandas-part2.py

--- data-manipulation-with-pandas-part2.py
+++ data-manipulation-with-pandas-part2.py
@@ -233,3 +233,35 @@ print('Swapped data:\n', data_swap)
 # [3] Melakukan sort_index pada stacking dataframe
 data_sort = data_swap.sort_index()
 print('Sorted data:\n', data_sort)
+
+"""
+12.
+"""
+import pandas as pd
+# Load data global_air_quality.csv
+global_air_quality = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv')
+print('Lima data teratas:\n', global_air_quality.head())
+# Melakukan pengecekan terhadap data
+print('Info global_air_quality:\n', global_air_quality.info())
+# Melakukan count tanpa groupby
+print('Count tanpa groupby:\n', global_air_quality.count())
+# Melakukan count dengan groupby 
+gaq_groupby_count = global_air_quality.groupby('source_name').count()
+print('Count dengan groupby (5 data teratas):\n', gaq_groupby_count.head())
+
+"""
+13. Groupby
+"""
+
+import pandas as pd
+# Load data global_air_quality.csv
+gaq = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv')
+# Create variabel pollutant 
+pollutant = gaq[['country', 'city', 'pollutant', 'value']].pivot_table(index=['country', 'city'],columns='pollutant').fillna(0)
+print('Data pollutant (5 teratas):\n', pollutant.head())
+# [1] Group berdasarkan country dan terapkan aggregasi mean
+pollutant_mean = pollutant.groupby('country').mean()
+print('Rata-rata pollutant (5 teratas):\n', pollutant_mean.head())
+# [2] Group berdasarkan country dan terapkan aggregasi std
+pollutant_std = pollutant.groupby('country').std().fillna(0)
+print('Standar deviasi pollutant (5 teratas):\n', pollutant_std.head())
