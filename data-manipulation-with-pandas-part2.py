42791b94909a6b12d4cd6e066dc0040c668843a7
Hamdan Romadhona <104075365+hamdanromad@users.noreply.github.com>
2022-07-02 18:41:37 +0700

    Update data-manipulation-with-pandas-part2.py

--- data-manipulation-with-pandas-part2.py
+++ data-manipulation-with-pandas-part2.py
@@ -265,3 +265,19 @@ print('Rata-rata pollutant (5 teratas):\n', pollutant_mean.head())
 # [2] Group berdasarkan country dan terapkan aggregasi std
 pollutant_std = pollutant.groupby('country').std().fillna(0)
 print('Standar deviasi pollutant (5 teratas):\n', pollutant_std.head())
+
+"""
+14. Groupby 2
+"""
+import pandas as pd
+# Load data https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv')
+# Create variabel pollutant 
+pollutant = gaq[['country','city','pollutant','value']].pivot_table(index=['country','city'],columns='pollutant').fillna(0)
+print('Data pollutant (5 teratas):\n', pollutant.head())
+# [3] Group berdasarkan country dan terapkan aggregasi sum
+pollutant_sum = pollutant.groupby('country').sum()
+print('Total pollutant (5 teratas):\n', pollutant_sum.head())
+# [4] Group berdasarkan country dan terapkan aggregasi nunique
+pollutant_nunique = pollutant.groupby('country').nunique()
+print('Jumlah unique value pollutant (5 teratas):\n', pollutant_nunique.head())
