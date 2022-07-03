002cea51c03e4702e3fbfc722560c04b1bed8849
Hamdan Romadhona <104075365+hamdanromad@users.noreply.github.com>
2022-07-03 19:49:44 +0700

    Create mini-project-data-manipulation-part2.py

---
+++ mini-project-data-manipulation-part2.py
@@ -0,0 +1,65 @@
+# Perusahaan membutuhkan perbandingan performa dari setiap 
+# cabang di berbagai kota. Ambil lima kota 
+# terbesar di Pulau Jawa untuk dianalisis dari 
+# segi order size, customer count, product 
+# count, brand count, dan GMV 
+# dalam basis bulanan.
+# """
+
+"""
+Performa Penjualan di Setiap Cabang - Part 1
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# [1]. Load masing-masing data dengan pandas
+retail_data1 = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/10%25_original_randomstate%3D42/retail_data_from_1_until_3_reduce.csv')
+retail_data2 = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/10%25_original_randomstate%3D42/retail_data_from_4_until_6_reduce.csv')
+retail_data3 = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/10%25_original_randomstate%3D42/retail_data_from_7_until_9_reduce.csv')
+retail_data4 = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/10%25_original_randomstate%3D42/retail_data_from_10_until_12_reduce.csv')
+
+# [2]. Pengecekan Data
+print('PENGECEKAN DATA\n\n')
+#      Cek data sekilas (tampilkan 5 baris teratas)
+print(retail_data1.head())
+#      Cek list kolom untuk semua dataframe
+print('Kolom retail_data1: %s' %retail_data1.columns)
+print('Kolom retail_data2: %s' %retail_data2.columns)
+print('Kolom retail_data3: %s' %retail_data3.columns)
+print('Kolom retail_data4: %s' %retail_data4.columns)
+#      Concat multiple dataframe menjadi 1 dataframe
+retail_table = pd.concat([retail_data1, retail_data2, retail_data3, retail_data4])
+print('\nJumlah baris:', retail_table.shape[0])
+#      Pengecekan dataframe info
+print('\nInfo:')
+print(retail_table.info())
+#      Pengecekan statistik deskriptif
+print('\nStatistik deskriptif:\n', retail_table.describe())
+
+"""
+Part 2
+"""
+# [3]. Transformasi Data
+print('TRANSFORMASI DATA\n\n')
+#      Memastikan data yang memiliki item_price < 0 atau total_price < 0
+cek = retail_table.loc[(retail_table['item_price'] < 0) | (retail_table['total_price'] < 0)]
+print('\nitem_price < 0 atau total_price < 0:\n', cek)
+#      Jika tidak masuk akal datanya dapat dibuang
+if cek.shape[0] != 0:
+	retail_table = retail_table.loc[(retail_table['item_price'] > 0) & (retail_table['total_price'] > 0)]
+#      Cek apakah masih ada order_id yang bernilai undefined dan delete row tersebut
+cek = retail_table.loc[retail_table['order_id'] == 'undefined']
+print('\norder_id yang bernilai undefined:\n', cek)
+#      Jika ada maka buang baris tersebut
+if cek.shape[0] != 0:
+	retail_table = retail_table.loc[retail_table['order_id'] != 'undefined']
+
+#      Transform order_id menjadi int64
+retail_table['order_id'] = retail_table['order_id'].astype('int64')
+#      Transform order_date menjadi datetime Pandas
+retail_table['order_date'] = pd.to_datetime(retail_table['order_date'])
+#      Cek dataframe info kembali untuk memastikan
+print('\nInfo:')
+print(retail_table.info())
+#      Cek statistik deskriptif kembali, untuk memastikan
+print('\nStatistik deskriptif:\n', retail_table.describe())
