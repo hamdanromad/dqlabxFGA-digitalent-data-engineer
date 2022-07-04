cdfebee38adf7507cb298baa365ad8c90042565d
Hamdan Romadhona <104075365+hamdanromad@users.noreply.github.com>
2022-07-04 10:15:44 +0700

    Update data-science-in-telco-data-cleansing.py

--- data-science-in-telco-data-cleansing.py
+++ data-science-in-telco-data-cleansing.py
@@ -58,3 +58,118 @@ print('Hasil jumlah ID Customer yang sudah dihilangkan duplikasinya (distinct) a
 """
 Mengatasi Missing Values dengan Penghapusan Rows
 """
+print('Total missing values data dari kolom Churn', df_load['Churn'].isnull().sum())
+# Dropping all Rows with spesific column (churn)
+df_load.dropna(subset=['Churn'], inplace=True)
+print('Total Rows dan kolom Data setelah dihapus data Missing Values adalah', df_load.shape)
+
+"""
+Mengatasi Missing Values dengan Pengisian Nilai tertentu
+"""
+# Tenure pihak data modeller meminta setiap rows yang memiliki missing values untuk lama berlangganan di isi dengan 11.
+# Variable yang bersifat numeric selain Tenure di isi dengan median dari masing-masing variable tersebut.
+# Tentukan:
+# 1. Apakah masih ada data yang missing values
+# 2. Jumlah missing values dari masing-masing variable
+# 3. Tangani missing values-nya
+print('Status Missing Values :',df_load.isnull().values.any())
+print('\nJumlah Missing Values masing-masing kolom, adalah:')
+print(df_load.isnull().sum().sort_values(ascending=False))
+
+#handling missing values Tenure fill with 11
+df_load['tenure'].fillna(11, inplace=True)
+#Loop
+#Handling missing values num vars (except Tenure)
+for col_name in list(['MonthlyCharges', 'TotalCharges']):
+    #write your command here
+	median = df_load[col_name].median()
+	df_load[col_name].fillna(median, inplace=True)
+	
+print('\nJumlah Missing Values setelah di imputer datanya, adalah:')
+print(df_load.isnull().sum().sort_values(ascending=False))
+
+"""
+Mendeteksi adanya Outlier (Boxplot)
+"""
+print('\nPersebaran data sebelum ditangani Outlier: ')
+print(df_load[['tenure','MonthlyCharges','TotalCharges']].describe())
+
+# Creating Box Plot
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Misal untuk kolom tenure
+plt.figure()
+sns.boxplot(x=df_load['tenure'])
+plt.show()
+# dan seterusnya untuk kedua kolom yang tersisa secara berurut
+plt.figure()
+sns.boxplot(x=df_load['MonthlyCharges'])
+plt.show()
+plt.figure()
+sns.boxplot(x=df_load['TotalCharges'])
+plt.show()
+
+"""
+Mengatasi Outlier
+"""
+# Your code goes here
+# Handling with IQR
+Q1 = (df_load[['tenure', 'MonthlyCharges', 'TotalCharges']]).quantile(0.25)
+Q3 = (df_load[['tenure', 'MonthlyCharges', 'TotalCharges']]).quantile(0.75)
+
+IQR = Q3 - Q1
+maximum = Q3 + (1.5*IQR)
+print('Nilai Maximum dari masing-masing Variable adalah: ')
+print(maximum)
+minimum = Q1 - (1.5*IQR)
+print('\nNilai Minimum dari masing-masing Variable adalah: ')
+print(minimum)
+
+more_than = (df_load > maximum)
+lower_than = (df_load < minimum)
+df_load = df_load.mask(more_than, maximum, axis=1)
+df_load = df_load.mask(lower_than, minimum, axis=1)
+
+print('\nPersebaran data setelah ditangani Outlier: ')
+print(df_load[['tenure', 'MonthlyCharges', 'TotalCharges']].describe())
+
+"""
+Mendeteksi Nilai yang Tidak Standar
+"""
+#Loop
+for col_name in list([
+  'gender',
+  'SeniorCitizen',
+  'Partner',
+  'Dependents',		
+  'PhoneService',
+  'MultipleLines',
+  'InternetService',
+  'OnlineSecurity',
+  'OnlineBackup',
+  'DeviceProtection',
+  'TechSupport',
+  'StreamingTV',
+  'StreamingMovies',
+  'Contract',
+  'PaperlessBilling',
+  'PaymentMethod',
+  'Churn'
+]
+):
+	print('\nUnique Values Count \033[1m' + 'Before Standardized \033[0m Variable', col_name)
+	print(df_load[col_name].value_counts())
+
+"""
+Menstandarisasi Variable Kategorik
+"""
+
+df_load = df_load.replace(
+  ['Wanita', 'Laki-Laki', 'Churn', 'Iya'], 
+  ['Female', 'Male', 'Yes', 'Yes']
+)
+#Loop
+for col_name in list(['gender', 'Dependents', 'Churn']):
+	print('\nUnique Values Count \033[1m' + 'After Standardized \033[0mVariable', col_name)
+	print(df_load[col_name].value_counts())
